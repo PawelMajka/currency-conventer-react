a52459df1403a1be5b158757b6973cab6a999a0a
Pawel Majka <pawelmajka1989@gmail.com>
2023-06-25 12:59:57 +0200

    added curriencies.js to App component

---
+++ src/App/curriencies.js
@@ -0,0 +1,21 @@
+export const currencies = [
+
+    {
+        name: "Dolar amerykański",
+        short: "USD",
+        rate: 4.6444,
+    },
+
+    {
+        name: "Euro",
+        short: "EUR",
+        rate: 4.7222,
+    },
+
+    {
+        name: "Funt brytyjski",
+        short: "EGBP",
+        rate: 5.2851,
+    },
+
+];
