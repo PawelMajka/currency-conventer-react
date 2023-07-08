e742f8c0bfb00579ac5d8a37b36bb8527ff2a2da
Pawel Majka <pawelmajka1989@gmail.com>
2023-07-08 18:07:15 +0200

    added index.js to Footer component

---
+++ src/App/Footer/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Footer = () => (
+    <div>
+        <footer className="footer">
+            ©Copyright 2022 - wszystkie prawa zastrzeżone!!!
+        </footer>
+    </div>
+);
+
+export default Footer;
